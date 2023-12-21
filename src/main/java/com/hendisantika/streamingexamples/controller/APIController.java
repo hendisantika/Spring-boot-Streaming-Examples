5f82c186643218e6da1077525968503aa7957c14
hendisantika <hendisantika@yahoo.co.id>
2023-12-21 14:42:38 +0700

    Add streamData in APIController 🫸🌀✏️📗🐧🐳

--- src/main/java/com/hendisantika/streamingexamples/controller/APIController.java
+++ src/main/java/com/hendisantika/streamingexamples/controller/APIController.java
@@ -1,7 +1,11 @@
 package com.hendisantika.streamingexamples.controller;
 
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
 /**
  * Created by IntelliJ IDEA.
@@ -16,4 +20,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/stream")
 @RestController
 public class APIController {
+    @GetMapping(value = "/data")
+    public ResponseEntity<StreamingResponseBody> streamData() {
+        StreamingResponseBody responseBody = response -> {
+            for (int i = 1; i <= 1000; i++) {
+                try {
+                    Thread.sleep(10);
+                    response.write(("Data stream line - " + i + "\n").getBytes());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        return ResponseEntity.ok()
+                .contentType(MediaType.TEXT_PLAIN)
+                .body(responseBody);
+    }
 }
