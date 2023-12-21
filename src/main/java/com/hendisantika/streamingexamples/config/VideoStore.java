572bba270a8952ab6c2201575bef852ec4a142ff
hendisantika <hendisantika@yahoo.co.id>
2023-12-21 14:45:42 +0700

    Add VideoStore 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/com/hendisantika/streamingexamples/config/VideoStore.java
@@ -0,0 +1,18 @@
+package com.hendisantika.streamingexamples.config;
+
+import org.springframework.content.commons.repository.Store;
+import org.springframework.content.rest.StoreRestResource;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : Spring-boot-Streaming-Examples
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/21/23
+ * Time: 14:45
+ * To change this template use File | Settings | File Templates.
+ */
+@StoreRestResource(path = "/videos")
+public interface VideoStore extends Store<String> {
+}
