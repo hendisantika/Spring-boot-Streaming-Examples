3af668fe77fe5d96f5d70003b651dedfa3e64d39
hendisantika <hendisantika@yahoo.co.id>
2023-12-21 14:40:16 +0700

    Add getAsyncExecutor in AsyncConfig 🫸🌀✏️📗🐧🐳

--- src/main/java/com/hendisantika/streamingexamples/config/AsyncConfig.java
+++ src/main/java/com/hendisantika/streamingexamples/config/AsyncConfig.java
@@ -5,10 +5,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.aop.interceptor.SimpleAsyncUncaughtExceptionHandler;
 import org.springframework.boot.autoconfigure.task.TaskExecutionProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
 
 /**
  * Created by IntelliJ IDEA.
@@ -35,5 +39,20 @@ public class AsyncConfig implements AsyncConfigurer {
     @Override
     public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
         return new SimpleAsyncUncaughtExceptionHandler();
+    }
+
+    @Override
+    @Bean(name = "taskExecutor")
+    public Executor getAsyncExecutor() {
+        log.debug("Create Async Task Executor");
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+//        executor.setCorePoolSize(taskExecutionProperties.getPool().getCoreSize());
+//        executor.setMaxPoolSize(taskExecutionProperties.getPool().getMaxSize());
+//        executor.setQueueCapacity(taskExecutionProperties.getPool().getQueueCapacity());
+//        executor.setThreadNamePrefix(taskExecutionProperties.getThreadNamePrefix());
+        executor.setCorePoolSize(1);
+        executor.setMaxPoolSize(2);
+        executor.setQueueCapacity(50);
+        return executor;
     }
 }
