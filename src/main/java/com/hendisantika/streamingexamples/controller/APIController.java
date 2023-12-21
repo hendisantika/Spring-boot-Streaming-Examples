fde72d6f5e1fbd3d85bd9d0b72bde12da20ef149
hendisantika <hendisantika@yahoo.co.id>
2023-12-21 14:42:25 +0700

    Add APIController 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/com/hendisantika/streamingexamples/controller/APIController.java
@@ -0,0 +1,19 @@
+package com.hendisantika.streamingexamples.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : Spring-boot-Streaming-Examples
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/21/23
+ * Time: 14:42
+ * To change this template use File | Settings | File Templates.
+ */
+@RequestMapping("/api/stream")
+@RestController
+public class APIController {
+}
