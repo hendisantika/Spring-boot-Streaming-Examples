ec09d56673423dd95ea52d1bdcd6a21379a4ff32
hendisantika <hendisantika@yahoo.co.id>
2023-12-21 14:40:26 +0700

    Add getTaskExecutor in AsyncConfig 🫸🌀✏️📗🐧🐳

--- src/main/java/com/hendisantika/streamingexamples/config/AsyncConfig.java
+++ src/main/java/com/hendisantika/streamingexamples/config/AsyncConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ConcurrentTaskExecutor;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.Executor;
@@ -54,5 +55,10 @@ public class AsyncConfig implements AsyncConfigurer {
         executor.setMaxPoolSize(2);
         executor.setQueueCapacity(50);
         return executor;
+    }
+
+    @Bean
+    protected ConcurrentTaskExecutor getTaskExecutor() {
+        return new ConcurrentTaskExecutor(this.getAsyncExecutor());
     }
 }
