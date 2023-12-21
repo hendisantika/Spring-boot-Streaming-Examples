8adba4fad6e10069212e68f39289663f152b9024
hendisantika <hendisantika@yahoo.co.id>
2023-12-21 14:37:01 +0700

    Create starter project 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/com/hendisantika/streamingexamples/SpringBootStreamingExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.streamingexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStreamingExamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootStreamingExamplesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/streamingexamples/SpringBootStreamingExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.streamingexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStreamingExamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
